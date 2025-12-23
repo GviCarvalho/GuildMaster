3bd2be69f840976fdcdb5dfc27fd1fbb1780d2b2
Guilherme Carvalho <77419868+GviCarvalho@users.noreply.github.com>
2025-12-23 03:48:19 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/GuildMaster/',
   build: {
     outDir: 'dist',
     sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
   },
 });
